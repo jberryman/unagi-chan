4fe9e197aad8876593196ea0afda3926dc3bf3c8
Brandon Simmons <brandon.m.simmons@gmail.com>
2014-01-04 18:03:39 -0500

    Start cleaning up Chan001, but looks like bitrot

--- tests/Chan001.hs
+++ tests/Chan001.hs
@@ -2,11 +2,11 @@ module Chan001 where
 
 import Debug.QuickCheck
 import System.IO.Unsafe
-import Control.Concurrent.Chan
+import Control.Concurrent.Chan.Split
 import Control.Concurrent
 import Control.Monad
 
-data Action = NewChan | ReadChan | WriteChan Int | IsEmptyChan | ReturnInt Int
+data Action = NewChan | ReadChan | WriteChan Int | ReturnInt Int
 	    | ReturnBool Bool
   deriving (Eq,Show)
 
@@ -19,16 +19,8 @@ main = do
 
 testChan :: IO ()
 testChan = do
-  quickCheck prop_NewIs_NewRet
-  quickCheck prop_NewWriteIs_NewRet
   quickCheck prop_NewWriteRead_NewRet
 
-
-prop_NewIs_NewRet = 
-  [NewChan,IsEmptyChan] =^ [NewChan,ReturnBool True]
-
-prop_NewWriteIs_NewRet n = 
-  [NewChan,WriteChan n,IsEmptyChan] =^ [NewChan,WriteChan n,ReturnBool False]
 
 prop_NewWriteRead_NewRet n = 
   [NewChan,WriteChan n,ReadChan] =^ [NewChan,ReturnInt n]
@@ -41,7 +33,7 @@ perform (a:as) =
   case a of
     ReturnInt v	 -> liftM (\(b,l) -> (b,v:l)) (perform as)
     ReturnBool v -> liftM (\(b,l) -> (v:b,l)) (perform as)
-    NewChan      -> newChan >>= \chan -> perform' chan as    
+    NewChan      -> newSplitChan >>= \chan -> perform' chan as    
     _		 -> error $ "Please use NewChan as first action"
 
 
@@ -55,8 +47,6 @@ perform' chan (a:as) =
     ReadChan     -> liftM2 (\v (b,l) -> (b,v:l)) (readChan chan) 
 			        (perform' chan as)
     WriteChan n  -> writeChan chan n >> perform' chan as
-    IsEmptyChan  -> liftM2 (\v (b,l) -> (v:b,l)) (isEmptyChan chan)
-				(perform' chan as)
     _		     -> error $ "If you want to use " ++ show a 
 				++ " please use the =^ operator"
 
@@ -69,7 +59,6 @@ actions =
 actions' :: Int -> Gen [Action]
 actions' contents =
   oneof ([return [],
-	  liftM (IsEmptyChan:) (actions' contents),
 	  liftM2 (:) (liftM WriteChan arbitrary) (actions' (contents+1))]
 	  ++
 	  if contents==0
@@ -107,5 +96,3 @@ delta i (WriteChan _:as) = delta (i+1) as
 delta i (ReadChan:as) = delta (if i==0
 				  then error "read on empty Chan"
 				  else i-1) as
-
-delta i (IsEmptyChan:as) = delta i as
