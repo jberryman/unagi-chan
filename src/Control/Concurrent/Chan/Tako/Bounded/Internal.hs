e6e4776e28fa1450869d9764081d168c923a020f
Brandon Simmons <brandon.m.simmons@gmail.com>
2014-06-07 14:50:57 -0400

    MVarrArray: improve assertions

--- src/Control/Concurrent/Chan/Tako/Bounded/Internal.hs
+++ src/Control/Concurrent/Chan/Tako/Bounded/Internal.hs
@@ -6,21 +6,17 @@ module Control.Concurrent.Chan.Tako.Bounded.Internal (
     ) where
 
 import Control.Concurrent.MVar
---import Data.IORef
 import Control.Exception
---import Control.Monad.Primitive(RealWorld)
---import Data.Atomics.Counter.Fat
---import Data.Atomics
 import qualified Data.Primitive as P
 import Control.Monad
 import Control.Applicative
 import Data.Bits
---import Data.Typeable(Typeable)
---import GHC.Exts(inline)
---import GHC.Conc(getNumProcessors)
+import Data.Word
+
+
 
 -- We use this as part of some other implementations as well as Tako.Bounded
--- TODO more efficient unboxed implementation possible?
+-- TODO more efficient unboxed implementation possible with CMM?
 data MVarArray a = MVarArray !Int !(P.Array (MVar a))
 --                              \ array size - 1, for bitwise mod
 
@@ -36,22 +32,25 @@ newMVarArray !sizeDirty = do
 
     MVarArray sizeMn1 <$> P.unsafeFreezeArray mArr
 
--- Put to the index specified, wrapping if larger than array
+-- TODO rename to take/put:
+-- Put to the index specified, wrapping if larger than array; negative `n` OK.
 writeMVarArray :: MVarArray a -> Int -> a -> IO ()
 {-# INLINE writeMVarArray #-}
 writeMVarArray !(MVarArray sizeMn1 arr) !n = 
-    assert (n >= 0) $
       putMVar (P.indexArray arr (n .&. sizeMn1))
 
 readMVarArray :: MVarArray a -> Int -> IO a
 {-# INLINE readMVarArray #-}
 readMVarArray !(MVarArray sizeMn1 arr) !n =
-    assert (n >= 0) $
       takeMVar (P.indexArray arr (n .&. sizeMn1))
 
 -- Not particularly fast; if needs moar fast see
 --   http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
+-- 
 nextHighestPowerOfTwo :: Int -> Int
 nextHighestPowerOfTwo 0 = 1
 nextHighestPowerOfTwo n =  
-    2 ^ (ceiling (logBase 2 $ fromIntegral $ abs n :: Float) :: Int)
+    let !nhp2 = 2 ^ (ceiling (logBase 2 $ fromIntegral $ abs n :: Float) :: Int)
+        -- ensure return value is actually a positive power of 2:
+     in assert (nhp2 > 0 && popCount (fromIntegral nhp2 :: Word) == 1)
+            nhp2
