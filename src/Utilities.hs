1a4818f7e1583ae27b3e8b823dd6e7404de683fc
Brandon Simmons <brandon.m.simmons@gmail.com>
2014-06-28 14:22:32 -0400

    A simple ordered assoc list IndexedMVar looks fine for us; test was hanging due to memory model issues

--- src/Utilities.hs
+++ src/Utilities.hs
@@ -15,7 +15,6 @@ import Data.Bits
 import Data.Word
 import Data.Atomics
 import Data.IORef
-import Control.Monad
 
 -- For now: a reverse-ordered assoc list; an IntMap might be better
 newtype IndexedMVar a = IndexedMVar (IORef [(Int, MVar a)])
@@ -25,60 +24,46 @@ newIndexedMVar = IndexedMVar <$> newIORef []
 
 
 
--- these really suck;sorry.
+-- these really suck; sorry.
+
 readMVarIx :: IndexedMVar a -> Int -> IO a
 {-# INLINE readMVarIx #-}
-readMVarIx (IndexedMVar v) i = do
-    -- we expect to have to create this, so do optimistically:
+readMVarIx mvIx i = do
+    readMVar =<< getMVarIx mvIx i
+
+putMVarIx :: IndexedMVar a -> Int -> a -> IO ()
+{-# INLINE putMVarIx #-}
+putMVarIx mvIx i a = do
+    flip putMVar a =<< getMVarIx mvIx i
+
+getMVarIx :: IndexedMVar a -> Int -> IO (MVar a)
+{-# INLINE getMVarIx #-}
+getMVarIx (IndexedMVar v) i = do
+    -- We're right to optimistically create this for readMVarIx, but throw this
+    -- away for most putMVarIx (from writers), probably.
     mv <- newEmptyMVar
     tk0 <- readForCAS v
     let go tk = do
             let !xs = peekTicket tk
             case findInsert i mv xs of
-                 Left alreadyPresentMVar -> readMVar alreadyPresentMVar
+                 Left alreadyPresentMVar -> return alreadyPresentMVar
                  Right xs' -> do 
                     (success,newTk) <- casIORef v tk xs'
                     if success 
-                        then readMVar mv
+                        then return mv
                         else go newTk
     go tk0
 
-findInsert :: Int -> MVar a -> [(Int,MVar a)] -> Either (MVar a) [(Int,MVar a)]
+-- Reverse-sorted:
+findInsert :: Int -> mvar -> [(Int,mvar)] -> Either mvar [(Int,mvar)]
 {-# INLINE findInsert #-}
 findInsert i mv = ins where
-    ins [] = Right [a] 
-    ins xss@((i',x):xs) = case compare i i' of
+    ins [] = Right [(i,mv)] 
+    ins xss@((i',x):xs) = 
+               case compare i i' of
                     GT -> Right $ (i,mv):xss
                     EQ -> Left x
                     LT -> fmap ((i',x):) $ ins xs
-
-find :: Int -> [(Int,MVar a)] -> Maybe (MVar a)
-{-# INLINE find #-}
-find k = go where
-    go [] = Nothing
-    go ((i',x):xs) | i == i' = Just x
-                   | i < i' = Nothing
-                   | otherwise = go xs
-
-putMVarIx :: IndexedMVar a -> Int -> a -> IO ()
-{-# INLINE putMVarIx #-}
-putMVarIx (IndexedMVar v) i a = do
-    tk0 <- readForCAS v
-    case find i $ peekTicket tk0 of
-         Just mv -> putMVar mv a
-         Nothing -> do
-            mv <- newMVar a
-            let go tk = do
-                    let !xs = peekTicket tk
-                    (success,newTk) <- casIORef v tk ((i,mv):xs)
-                    unless success $ 
-                        case findInsert i mv $ peekTicket newTk of
-                             Left alreadyPresentMVar -> 
-                                putMVar alreadyPresentMVar a
-                             Right xs' -> go newTk
-            go tk0 (
-    
-    
 
 
 -- Not particularly fast; if needs moar fast see
--- tests/IndexedMVar.hs
+++ tests/IndexedMVar.hs
@@ -9,29 +9,27 @@ import Data.IORef
 indexedMVarMain :: IO ()
 indexedMVarMain = do
     putStr "Test indexedMVar... "
-    replicateM_ 100 $ 
-        indexedMVarTest 32
+    replicateM_ 1000 $ 
+        indexedMVarTest 128
     putStrLn "OK"
 
--- TODO quickcheck
 indexedMVarTest :: Int -> IO ()
 indexedMVarTest n = do
     let xs = [0..n]
     mvIx <- newIndexedMVar
     start <- newIORef (0::Int)
     _ <- forkIO $ do
-           _ <- writeIORef start 1
+           writeIORef start 1
            mapM_ (\x-> putMVarIx mvIx x x `onException` (putStr "In writer: ")) xs
     
     busyWait start
     mapM_ (\x-> (readMVarIx mvIx x `onException` (putStr "In reader: ")) >>= assertEq x) xs
-        where assertEq x x' | x == x' = return ()
-                            | otherwise = error $ (show x)++"/="++(show x')
-
+        where assertEq x x' = when (x /= x') $ error $ (show x)++"/="++(show x')
 
 -- we should really abstract this out and use elsewhere
 busyWait :: IORef Int -> IO ()
 busyWait ref = busy where
     busy = do
-        n <- readIORef ref
+         -- N.B. w/ readIORef we loop forever:
+        n <- atomicModifyIORef ref (\x-> (x,x)) 
         unless (n == 1) busy
