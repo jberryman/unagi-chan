0aabecc01b742fba2ba4fcebbd7dc0bc682021b8
Brandon Simmons <brandon.m.simmons@gmail.com>
2014-08-10 16:37:09 -0400

    move constants to bottom of file

--- src/Control/Concurrent/Chan/Unagi/Internal.hs
+++ src/Control/Concurrent/Chan/Unagi/Internal.hs
@@ -28,12 +28,6 @@ import GHC.Exts(inline)
 
 
 
--- Number of reads on which to spin for new segment creation.
--- Back-of-envelope (time_to_create_new_segment / time_for_read_IOref) + margin.
--- See usage site.
-nEW_SEGMENT_WAIT :: Int
-nEW_SEGMENT_WAIT = round (((14.6::Float) + 0.3*fromIntegral sEGMENT_LENGTH) / 3.7) + 10
-
 -- | The write end of a channel created with 'newChan'.
 data InChan a = InChan !(Ticket (Cell a)) !(ChanEnd a)
     deriving Typeable
@@ -82,19 +76,6 @@ type StreamSegment a = P.MutableArray RealWorld (Cell a)
 --     Blocking
 data Cell a = Empty | Written a | Blocking !(MVar a)
 
-
--- Constant for now: back-of-envelope considerations:
---   - making most of constant factor for cloning array of *any* size
---   - make most of overheads of moving to the next segment, etc.
---   - provide enough runway for creating next segment when 32 simultaneous writers 
---   - the larger this the larger one-time cost for the lucky writer
---   - as arrays collect in heap, performance might suffer, so bigger arrays
---     give us a constant factor edge there. see:
---       http://stackoverflow.com/q/23462004/176841
---
-sEGMENT_LENGTH :: Int
-{-# INLINE sEGMENT_LENGTH #-}
-sEGMENT_LENGTH = 1024 -- NOTE: THIS MUST REMAIN A POWER OF 2!
 
 -- NOTE In general we'll have two segments allocated at any given time in
 -- addition to the segment template, so in the worst case, when the program
@@ -319,16 +300,40 @@ newSegmentSource = do
 -- ----------
 
 
--- TODO MOVE THIS INTO UTILITIES, ONCE WE DECIDE TO SETTLE ON SAME CONSTANTS
---      (and move those into a "Constants" module), AND FACTOR OUT FROM
---      Unagi.Unboxed, too.
-
-lOG_SEGMENT_LENGTH, sEGMENT_LENGTH_MN_1 :: Int
-lOG_SEGMENT_LENGTH = round $ logBase (2::Float) $ fromIntegral sEGMENT_LENGTH -- or bit shifts in loop
-sEGMENT_LENGTH_MN_1 = sEGMENT_LENGTH - 1
-
 divMod_sEGMENT_LENGTH :: Int -> (Int,Int)
 {-# INLINE divMod_sEGMENT_LENGTH #-}
 divMod_sEGMENT_LENGTH n = let d = n `unsafeShiftR` lOG_SEGMENT_LENGTH
                               m = n .&. sEGMENT_LENGTH_MN_1
                            in d `seq` m `seq` (d,m)
+
+-- --------------------------------------------------------------------
+--   CONSTANTS
+-- --------------------------------------------------------------------
+
+-- TODO MOVE THIS INTO UTILITIES, ONCE WE DECIDE TO SETTLE ON SAME CONSTANTS
+--      (and move those into a "Constants" module), AND FACTOR OUT FROM
+--      Unagi.Unboxed, too.
+
+
+-- Constant for now: back-of-envelope considerations:
+--   - making most of constant factor for cloning array of *any* size
+--   - make most of overheads of moving to the next segment, etc.
+--   - provide enough runway for creating next segment when 32 simultaneous writers 
+--   - the larger this the larger one-time cost for the lucky writer
+--   - as arrays collect in heap, performance might suffer, so bigger arrays
+--     give us a constant factor edge there. see:
+--       http://stackoverflow.com/q/23462004/176841
+--
+sEGMENT_LENGTH :: Int
+{-# INLINE sEGMENT_LENGTH #-}
+sEGMENT_LENGTH = 1024 -- NOTE: THIS MUST REMAIN A POWER OF 2!
+
+-- Number of reads on which to spin for new segment creation.
+-- Back-of-envelope (time_to_create_new_segment / time_for_read_IOref) + margin.
+-- See usage site.
+nEW_SEGMENT_WAIT :: Int
+nEW_SEGMENT_WAIT = round (((14.6::Float) + 0.3*fromIntegral sEGMENT_LENGTH) / 3.7) + 10
+
+lOG_SEGMENT_LENGTH, sEGMENT_LENGTH_MN_1 :: Int
+lOG_SEGMENT_LENGTH = round $ logBase (2::Float) $ fromIntegral sEGMENT_LENGTH -- or bit shifts in loop
+sEGMENT_LENGTH_MN_1 = sEGMENT_LENGTH - 1
