401f9af2193964c846cc66240fa68f644d860524
Brandon Simmons <brandon.m.simmons@gmail.com>
2014-12-12 21:30:07 -0500

    NoBlocking.Unboxed benchmarks

--- benchmarks/single.hs
+++ benchmarks/single.hs
@@ -6,6 +6,7 @@ import qualified Control.Concurrent.Chan.Unagi as U
 import qualified Control.Concurrent.Chan.Unagi.Unboxed as UU
 import qualified Control.Concurrent.Chan.Unagi.Bounded as UB
 import qualified Control.Concurrent.Chan.Unagi.NoBlocking as UN
+import qualified Control.Concurrent.Chan.Unagi.NoBlocking.Unboxed as UNU
 #ifdef COMPARE_BENCHMARKS
 import Control.Concurrent.Chan
 import Control.Concurrent.STM
@@ -29,6 +30,7 @@ main = do
   (fastEmptyUUI,fastEmptyUUO) <- UU.newChan
   (fastEmptyUBI,fastEmptyUBO) <- UB.newChan 1024 -- only needs to be 1, but do apples-to-apples by matching sEGMENT_SIZE of other implementations
   (fastEmptyUNI,fastEmptyUNO) <- UN.newChan
+  (fastEmptyUNUI,fastEmptyUNUO) <- UNU.newChan
 #ifdef COMPARE_BENCHMARKS
   chanEmpty <- newChan
   tqueueEmpty <- newTQueueIO
@@ -45,6 +47,7 @@ main = do
         , bench "unagi-chan Unagi.Bounded 1024" $ nfIO (UB.writeChan fastEmptyUBI (0::Int) >> UB.readChan fastEmptyUBO) -- TODO comparing Int writing to (). Change?
         , bench "unagi-chan Unagi.Bounded 1024 with tryWriteChan" $ nfIO (UB.tryWriteChan fastEmptyUBI (0::Int) >> UB.readChan fastEmptyUBO) -- TODO comparing Int writing to (). Change?
         , bench "unagi-chan Unagi.NoBlocking" $ nfIO (UN.writeChan fastEmptyUNI (0::Int) >> tryReadChanErrUN fastEmptyUNO) -- TODO comparing Int writing to (). Change?
+        , bench "unagi-chan Unagi.NoBlocking.Unboxed" $ nfIO (UNU.writeChan fastEmptyUNUI (0::Int) >> tryReadChanErrUNU fastEmptyUNUO) -- TODO comparing Int writing to (). Change?
 #ifdef COMPARE_BENCHMARKS
         , bench "Chan" $ nfIO $ (writeChan chanEmpty () >> readChan chanEmpty)
         , bench "TQueue" $ nfIO $ (atomically (writeTQueue tqueueEmpty () >>  readTQueue tqueueEmpty))
@@ -63,6 +66,8 @@ main = do
               , bench "unagi-chan Unagi.Bounded" $ nfIO $ runtestSplitChanUB1 n
               , bench "unagi-chan Unagi.NoBlocking" $ nfIO $ runtestSplitChanUN1 n
               , bench "unagi-chan Unagi.NoBlocking Stream" $ nfIO $ runtestSplitChanUNStream1 n
+              , bench "unagi-chan Unagi.NoBlocking.Unboxed" $ nfIO $ runtestSplitChanUNU1 n
+              , bench "unagi-chan Unagi.NoBlocking.Unboxed Stream" $ nfIO $ runtestSplitChanUNUStream1 n
 #ifdef COMPARE_BENCHMARKS
               , bench "Chan" $ nfIO $ runtestChan1 n
               , bench "TQueue" $ nfIO $ runtestTQueue1 n
@@ -76,6 +81,8 @@ main = do
               , bench "unagi-chan Unagi.Bounded" $ nfIO $ runtestSplitChanUB2 n
               , bench "unagi-chan Unagi.NoBlocking" $ nfIO $ runtestSplitChanUN2 n
               , bench "unagi-chan Unagi.NoBlocking Stream" $ nfIO $ runtestSplitChanUNStream2 n
+              , bench "unagi-chan Unagi.NoBlocking.Unboxed" $ nfIO $ runtestSplitChanUNU2 n
+              , bench "unagi-chan Unagi.NoBlocking.Unboxed Stream" $ nfIO $ runtestSplitChanUNUStream2 n
 #ifdef COMPARE_BENCHMARKS
               , bench "Chan" $ nfIO $ runtestChan2 n
               , bench "TQueue" $ nfIO $ runtestTQueue2 n
@@ -92,6 +99,11 @@ tryReadChanErrUN :: UN.OutChan a -> IO a
 {-# INLINE tryReadChanErrUN #-}
 tryReadChanErrUN oc = UN.tryReadChan oc 
                     >>= UN.peekElement 
+                    >>= maybe (error "A read we expected to succeed failed!") return
+tryReadChanErrUNU :: UNU.UnagiPrim a=> UNU.OutChan a -> IO a
+{-# INLINE tryReadChanErrUNU #-}
+tryReadChanErrUNU oc = UNU.tryReadChan oc 
+                    >>= UNU.peekElement 
                     >>= maybe (error "A read we expected to succeed failed!") return
 
 
@@ -152,6 +164,51 @@ runtestSplitChanUNStream2 n = do
           eat str >>= writeAndEat (iter-1)
         
   writeAndEat (1000::Int) oStream
+
+
+-- unagi-chan Unagi.NoBlocking.Unboxed  --
+runtestSplitChanUNU1, runtestSplitChanUNU2 :: Int -> IO ()
+runtestSplitChanUNU1 n = do
+  (i,o) <- UNU.newChan
+  replicateM_ n $ UNU.writeChan i (0::Int)
+  replicateM_ n $ tryReadChanErrUNU o
+
+runtestSplitChanUNU2 n = do
+  (i,o) <- UNU.newChan
+  let n1000 = n `quot` 1000
+  replicateM_ 1000 $ do
+    replicateM_ n1000 $ UNU.writeChan i (0::Int)
+    replicateM_ n1000 $ tryReadChanErrUNU o
+
+-- unagi-chan Unagi.NoBlocking Stream --
+runtestSplitChanUNUStream1, runtestSplitChanUNUStream2 :: Int -> IO ()
+runtestSplitChanUNUStream1 n = do
+  (i,o) <- UNU.newChan
+  [ oStream ] <- UNU.streamChan 1 o
+  replicateM_ n $ UNU.writeChan i (0::Int)
+  -- consume until we hit empty:
+  let eat str = do
+          x <- UNU.tryReadStream str
+          case x of
+               UNU.Pending -> return ()
+               UNU.Cons _ str' -> eat str'
+  eat oStream
+
+runtestSplitChanUNUStream2 n = do
+  (i,o) <- UNU.newChan
+  [ oStream ] <- UNU.streamChan 1 o
+  let n1000 = n `quot` 1000
+  let eat str = do
+          x <- UNU.tryReadStream str
+          case x of
+               UNU.Pending -> return str
+               UNU.Cons _ str' -> eat str'
+      writeAndEat iter str = unless (iter <=0) $ do
+          replicateM_ n1000 $ UNU.writeChan i (0::Int)
+          eat str >>= writeAndEat (iter-1)
+        
+  writeAndEat (1000::Int) oStream
+
 
 
 -- unagi-chan Unagi Unboxed --
