eae9962841a817da105058009dc3490df925e5cf
Brandon Simmons <brandon.m.simmons@gmail.com>
2014-12-27 21:38:56 -0500

    In Counter.Fat we can just read and write from cell 0 since we're aligned to a cacheline

--- src/Data/Atomics/Counter/Fat.hs
+++ src/Data/Atomics/Counter/Fat.hs
@@ -12,12 +12,13 @@ import Data.Primitive.MachDeps(sIZEOF_INT)
 import Control.Monad.Primitive(RealWorld)
 import Data.Primitive.ByteArray
 import Data.Atomics(fetchAddByteArrayInt)
+import Control.Exception(assert)
 
 newtype AtomicCounter = AtomicCounter (MutableByteArray RealWorld)
 
-sIZEOF_CACHELINE , cACHELINE_PADDED_INT_IX  :: Int
+sIZEOF_CACHELINE :: Int
+{-# INLINE sIZEOF_CACHELINE #-}
 sIZEOF_CACHELINE   = 64
-cACHELINE_PADDED_INT_IX = (sIZEOF_CACHELINE `quot` 2) `quot` sIZEOF_INT
 
 newCounter :: Int -> IO AtomicCounter
 {-# INLINE newCounter #-}
@@ -25,15 +26,17 @@ newCounter n = do
     arr <- newAlignedPinnedByteArray 
                 sIZEOF_CACHELINE
                 sIZEOF_CACHELINE
-    writeByteArray arr cACHELINE_PADDED_INT_IX n
-    return (AtomicCounter arr)
+    writeByteArray arr 0 n
+    -- out of principle:
+    assert (sIZEOF_INT < sIZEOF_CACHELINE) $
+      return (AtomicCounter arr)
 
 incrCounter :: Int -> AtomicCounter -> IO Int
 {-# INLINE incrCounter #-}
 incrCounter incr (AtomicCounter arr) =
-    fetchAddByteArrayInt arr cACHELINE_PADDED_INT_IX incr
+    fetchAddByteArrayInt arr 0 incr
 
 readCounter :: AtomicCounter -> IO Int
 {-# INLINE readCounter #-}
 readCounter (AtomicCounter arr) = 
-    readByteArray arr cACHELINE_PADDED_INT_IX
+    readByteArray arr 0
